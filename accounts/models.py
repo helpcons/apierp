5c55ac3e41d864b6e358eedcc3416da55e305f71
Helder Pereira <helder_leg@hotmail.com>
2025-03-01 22:50:38 -0300

    criando o app companies e o relacionamento com users

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, Permission
+from companies.models import Enterprise
 
 class User(AbstractBaseUser):
     name = models.CharField(max_length=150)
@@ -10,3 +11,15 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.email
+
+class Group(models.Model):
+    name = models.CharField(max_length=85)
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
+
+class Group_Permissions(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
+
+class User_Groups(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
